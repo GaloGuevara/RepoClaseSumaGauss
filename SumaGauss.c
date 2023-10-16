5296c95a6a07f4b379b4175ea6a722f6149f595f
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2023-10-16 16:00:07 -0500

    Creación sistema ingreso numero entero y confirmación del numero ingresado

---
+++ SumaGauss.c
@@ -0,0 +1,12 @@
+/*Construir un programa en lenguaje C que calcule la suma de Gauss a partir de un numero entero ingresado por el usuario.
+
+Entregue la URL a su repositorio de GitHub. Debe evidenciar por lo menos 4 commits*/
+
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Ingrese un numero entero: ");
+    scanf("%d%*c", &num);
+    printf("\nEl numero ingresado es: %d", num);
+}
